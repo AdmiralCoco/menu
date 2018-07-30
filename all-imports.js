372e04be44dbd845b978805d1744b24faaf78b02
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2018-07-30 09:48:56 -0400

    Rename all-imports.html to all-imports.js

---
+++ all-imports.js
@@ -0,0 +1,7 @@
+import './d2l-menu.js';
+import './d2l-menu-item.js';
+import './d2l-menu-item-return.js';
+import './d2l-menu-item-link.js';
+import './d2l-menu-item-separator.js';
+import './d2l-menu-item-checkbox.js';
+import './d2l-menu-item-radio.js';
